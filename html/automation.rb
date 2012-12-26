b3c19417438d2abe717caa96980c4162a8f34a35
pauly <pauly@clarkeology.com>
2012-12-26 11:34:06 +0000

    more ruby home automation skills

--- html/automation.rb
+++ html/automation.rb
@@ -1,4 +1,41 @@
+#!/usr/bin/ruby
+
+host = '192.168.0.14' # wifi link ip
+sender = 533 # not sure how or why
+port = 9760 # wifi link port
 require 'socket'
-u1 = UDPSocket.new
-u1.bind("192.168.0.14", 9760)
-u1.send "533,!R2D1F0|", 0, "192.168.0.14", 9760
+case ARGV.length
+  when 3
+    room = ARGV[0]
+    device = ARGV[1]
+    state = ARGV[2]
+  when 2
+    room = ARGV[0]
+    device = ARGV[1]
+    state = 0
+  else
+    p "usage: #{__FILE__} lounge light on"
+    exit
+end
+case room
+  when 'lounge'
+    room = 'R1'
+  else
+    room = 'R1'
+end
+case device
+  when 'light'
+    device = 'D1'
+  else
+    device = 'D1'
+end
+case state
+  when 'off'
+    state = 0
+  when 'on'
+    state = 1
+end
+socket = UDPSocket.new
+socket.connect host, port
+socket.send sender.to_s + ",!" + room + device + "F" + state.to_s + "|", 0, "192.168.0.14", 9760
+p "sent " + state.to_s + " to " + room + device + "@" + host.to_s + ":" + port.to_s
