1d7be73a4186744134d34513e50f3fae56633932
pauly <pauly@clarkeology.com>
2012-12-27 17:38:04 +0000

    working command line now with external config file first draft

--- html/automation.rb
+++ html/automation.rb
@@ -1,9 +1,28 @@
 #!/usr/bin/ruby
 
-host = '192.168.0.14' # wifi link ip
+require 'socket'
+require 'yaml'
+require 'pp'
+
+host = '192.168.0.14' # wifi link ip, still to be moved to config
 sender = 533 # not sure how or why
 port = 9760 # wifi link port
-require 'socket'
+
+config = { 'room' => { }}
+r = 1
+YAML.load_file( 'config.yml' ).each do | room, devices |
+  # p r.to_s + ': ' + room + ' has ' + devices.length.to_s + ' devices'
+  config['room'][room] = {
+    'id' => 'R' + r.to_s,
+    'device' => { }
+  }
+  d = 1
+  devices.each do | device |
+    config['room'][room]['device'][device] = 'D' + d.to_s
+    d = d + 1
+  end
+  r = r + 1
+end
 case ARGV.length
   when 3
     room = ARGV[0]
@@ -12,20 +31,14 @@ case ARGV.length
   when 2
     room = ARGV[0]
     device = ARGV[1]
-    state = 0
+    state = 'on'
   else
     p "usage: #{__FILE__} lounge light on"
     exit
 end
-case room
-  when 'lounge'
-    room = 'R1'
-  else
-    room = 'R1'
-end
 case device
-  when 'light'
-    device = 'D1'
+  when 'lights'
+    device = 'D2'
   else
     device = 'D1'
 end
@@ -37,5 +50,9 @@ case state
 end
 socket = UDPSocket.new
 socket.connect host, port
-socket.send sender.to_s + ",!" + room + device + "F" + state.to_s + "|", 0, "192.168.0.14", 9760
+# p 'room is ' + room
+# p 'config[room] is ' + config['room'].to_s
+# p 'so config[room][' + room + '] is ' + config['room'][room].to_s
+# p 'so config[room][' + room + '][id] is ' + config['room'][room]['id']
+socket.send sender.to_s + ",!" + config['room'][room]['id'] + device + "F" + state.to_s + "|", 0, host, port
 p "sent " + state.to_s + " to " + room + device + "@" + host.to_s + ":" + port.to_s
