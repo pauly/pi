aab8cd3b3e0d024ca4da3d214e9e6306ba6cd51f
pauly <pauly@clarkeology.com>
2012-12-31 22:19:18 +0000

    tidied up, given up on this though to be honest

--- html/automation.php
+++ html/automation.php
@@ -1,6 +1,8 @@
 <?php
 
 /**
+ * My first go at computer home automation, now superceded.
+ * 
  * Turn the lights on like this
  * http://192.168.0.11/automation.php?room=2&device=1&state=on
  * 
@@ -11,10 +13,18 @@
  */
 
 $obj = new stdClass;
-switch ( $_GET['room'] ) {
+$obj->{'host'} = '192.168.0.14'; // ip address of your wifi link http://amzn.to/V7yPPK
+
+switch ( $_GET['room'] ) { // room aliases
   default:
     $_GET['room'] = 'R2';
 }
+
+switch ( $_GET['device'] ) { // device aliases
+  default:
+    $_GET['device'] = 'D1';
+}
+
 switch ( $_GET['state'] ) {
   case 'on':
     $_GET['state'] = 'F1';
@@ -22,26 +32,15 @@ switch ( $_GET['state'] ) {
   case 'off':
     $_GET['state'] = 'F0';
     break;
-  case '1':
-    $_GET['state'] = 'FdP1';
-    break;
   default:
-    $_GET['state'] = 'FdP' . round( $_GET['state'] * 0.32 );
+    $_GET['state'] = 'FdP' . ceil( $_GET['state'] * 0.32 );
 }
 
-switch ( $_GET['device'] ) {
-  default:
-    $_GET['device'] = 'D1';
-}
-
-$obj->{'host'} = '192.168.0.14';
-$obj->{'port'} = 9760;
 $sock = socket_create( AF_INET, SOCK_DGRAM, SOL_UDP );
-$code = 533;
-$obj->{'broadcast_string'} = sprintf( '%03d', $code ) . ',!' . $_GET['room'] . $_GET['device'] . $_GET['state'] . '|';
+$obj->{'broadcast_string'} = '666,!' . $_GET['room'] . $_GET['device'] . $_GET['state'] . '|';
 $obj->usage = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] . "?room=2&device=1&state=on";
 socket_set_option( $sock, SOL_SOCKET, SO_BROADCAST, 1 );
-socket_sendto( $sock, $obj->{'broadcast_string'}, strlen( $obj->{'broadcast_string'} ), 0, $obj->{'host'}, $obj->{'port'} );
+socket_sendto( $sock, $obj->{'broadcast_string'}, strlen( $obj->{'broadcast_string'} ), 0, $obj->{'host'}, 9670 );
 socket_close( $sock );
 
 echo json_encode( $obj );
